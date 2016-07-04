95fe249ff3522d291a6571ff038a4c2ce94ef92d
Nik_mmzd <ken-valya-nikita@ya.ru>
2016-07-04 16:25:30 +0300

    Мдя. Доделал эту папку, вроде

--- lang/russian/commands/commands-top10Command.js
+++ lang/russian/commands/commands-top10Command.js
@@ -1,2 +1,2 @@
-$.lang.register('top5.default', 'Top 5 $1: $2');
-$.lang.register('top5.points-disabled', 'Points are disabled.');
+$.lang.register('top5.default', 'Топ 5 $1: $2');
+$.lang.register('top5.points-disabled', 'Очки отключены.'); //Да, самый большой перевод I've ever seen
