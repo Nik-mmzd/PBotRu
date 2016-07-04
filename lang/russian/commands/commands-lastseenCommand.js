b81604155c00ac56bd6a5ff221ab6f1b5c76395d
Nik_mmzd <ken-valya-nikita@ya.ru>
2016-07-04 16:22:15 +0300

    Продолжаем перевод

--- lang/russian/commands/commands-lastseenCommand.js
+++ lang/russian/commands/commands-lastseenCommand.js
@@ -1,3 +1,3 @@
-$.lang.register('lastseen.404', 'I haven\'t seen $1 yet.');
-$.lang.register('lastseen.response', '$1 has last been seen on $2 @ $3');
-$.lang.register('lastseen.usage', 'Usage: !lastseen [username].');
+$.lang.register('lastseen.404', 'Я еще никогда не видел $1.');
+$.lang.register('lastseen.response', '$1 в последний раз видели в $2 @ $3');
+$.lang.register('lastseen.usage', 'Использование: !lastseen [username].');
